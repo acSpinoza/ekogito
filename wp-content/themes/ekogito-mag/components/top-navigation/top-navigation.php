75ecb2a2222ac96c88e1ccb71b0266d376d09bb4
Michou <michael.bontyes@edelman.com>
2016-02-20 00:47:54 -0500

    _s + uikit integration

---
+++ wp-content/themes/ekogito-mag/components/top-navigation/top-navigation.php
@@ -0,0 +1,5 @@
+
+<nav class="uk-navbar">
+    <ul class="uk-navbar-nav uk-hidden-small"><?php wp_nav_menu( array( 'theme_location' => 'top', 'menu_id' => 'top-menu' ) ); ?></ul>
+    <a href="#my-id" class="uk-navbar-toggle uk-visible-small" data-uk-offcanvas></a>
+</nav>
