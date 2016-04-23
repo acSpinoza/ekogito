8d97f079916a6ceb64f40627404ed2280a4bff83
root <root@ns396948.ip-5-196-68.eu>
2016-04-23 08:42:54 +0200

    ekogito frontage

---
+++ wp-content/themes/sparkling/footer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package sparkling
+ */
+?>
+			</div><!-- close .*-inner (main-content or sidebar, depending if sidebar is used) -->
+		</div><!-- close .row -->
+	</div><!-- close .container -->
+</div><!-- close .main-content -->
+
+	
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
