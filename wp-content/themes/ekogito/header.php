47f1ec5992a98d9b77676a37cce4c7e2096ca188
michaelbontyes <ekogitoco@gmail.com>
2016-12-02 12:58:44 -0500

    update

--- wp-content/themes/ekogito/header.php
+++ wp-content/themes/ekogito/header.php
@@ -21,16 +21,6 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCb8GnWDx_F9GNWUerSkEmIuyihKrP_RPI"></script>
 <link href="https://fonts.googleapis.com/css?family=Bungee+Outline|Cabin+Sketch:400,700|Fredericka+the+Great|Open+Sans+Condensed:300,300i|Open+Sans:300,300i" rel="stylesheet">
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-74185149-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
 <?php wp_head(); ?>
 </head>
 
